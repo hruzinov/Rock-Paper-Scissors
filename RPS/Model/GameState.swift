95205e2586ee1855c4af346ea8a948e9f56d6000
Evhen Gruzinov <evhen@gruzinov.com>
2022-08-24 21:21:47 +0300

    Initial Commit

---
+++ RPS/Model/GameState.swift
@@ -0,0 +1,12 @@
+//
+//  GameState.swift
+//  RPS
+//
+//  Created by Evhen Gruzinov on 24.08.2022.
+//
+
+import Foundation
+
+enum GameState {
+    case start, win, lose, draw, error
+}
---
+++ RPS/Model/Sign.swift
@@ -0,0 +1,32 @@
+//
+//  Sign.swift
+//  RPS
+//
+//  Created by Evhen Gruzinov on 24.08.2022.
+//
+
+import Foundation
+
+enum Sign {
+    case rock, paper, scissors, none
+    
+    var emoji: String {
+        switch self {
+        case .rock: return "👊"
+        case .paper: return "✋"
+        case .scissors: return "✌️"
+        case .none: return ""
+        }
+    }
+}
+
+func randomSign() -> Sign {
+    let sign = Int.random(in: 0...2)
+    if sign == 0 {
+        return .rock
+    } else if sign == 2 {
+        return .paper
+    } else {
+        return .scissors
+    }
+}
